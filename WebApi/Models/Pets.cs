2863298c84d0047126d23c6758923ee78a1a70df
Nematin <34038234+Nematin@users.noreply.github.com>
2017-12-12 20:27:51 +0500

    Добавил новую модель Pets

---
+++ WebApi/Models/Pets.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace WebApi.Models
+{
+    public class Pets: DbEntity
+    {
+        [Required]
+        public string Name{ get; set; }
+
+        [Required]
+        public string Role { get; set; }
+        public int Level { get; set; }
+        public int Experience { get; set; }
+
+        [JsonIgnore]
+        [XmlIgnore]
+        public int CharacterId { get; set; }
+    }
+}
